0b7f963e2ba180a2326419a9363b6488142412eb
naresy <nareshupadhayay74@gmail.com>
2024-05-04 09:07:58 -0700

    two_challenge

---
+++ Occuptional.sql
@@ -0,0 +1,13 @@
+SELECT 
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+    SELECT 
+        Name,
+        Occupation,
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM OCCUPATIONS
+) AS sub
+GROUP BY rn;
